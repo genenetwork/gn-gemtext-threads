19292ccd4d20c90ae1070e0b76bf5c115961f439
Arun Isaac <arunisaac@systemreboot.net>
2022-03-18 16:48:57 +0530

    Add aliases.
    * tissue.scm: Add #:aliases.

--- tissue.scm
+++ tissue.scm
@@ -2,4 +2,15 @@
 
 (tissue-configuration
  #:project "GeneNetwork"
- #:web-css "/style.css")
+ #:aliases '(("Alexander Kabui" "alex")
+             ("Arun Isaac" "aruni")
+             ("BonfaceKilz" "bonfacem")
+             ("Efraim Flashner" "efraimf")
+             ("Erik Garrison" "erikg")
+             ("Frederick Muriuki Muriithi" "Muriithi Frederick Muriuki" "fredm")
+             ("Pjotr Prins" "pjotrp")
+             ("Rob Williams" "robw")
+             ("acenteno")
+             ("jgart")
+             ("zsloan"))
+  #:web-css "/style.css")
