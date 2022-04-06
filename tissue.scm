6e7c6c2fc67c3022e0313f53b2e3ce714bec92d8
Arun Isaac <arunisaac@systemreboot.net>
2022-04-06 14:33:30 +0530

    tissue: Add #:web-files.
    * tissue.scm (#:web-files): New setting.

--- tissue.scm
+++ tissue.scm
@@ -13,4 +13,19 @@
              ("acenteno")
              ("jgart")
              ("zsloan"))
-  #:web-css "/style.css")
+ #:web-css "/style.css"
+ #:web-files (cons* (file "style.css"
+                          (copier "style.css"))
+                    (file "index.html"
+                          (skribe-exporter "index.skb"))
+                    (file "team.html"
+                          (skribe-exporter "team.skb"))
+                    (append (tag-pages)
+                            (filter-map (lambda (filename)
+                                          (and (string-suffix? ".gmi" filename)
+                                               (not (string=? (basename filename)
+                                                              "README.gmi"))
+                                               (file (replace-extension filename "html")
+                                                     (gemtext-exporter filename
+                                                                       (gemtext-reader)))))
+                                        (git-tracked-files)))))
