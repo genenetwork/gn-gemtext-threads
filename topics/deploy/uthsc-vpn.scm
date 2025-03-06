37ad139a6a62886278687c3da93cd89d2dfd1672
Pjotr Prins <pjotr.public01@thebird.nl>
2025-03-06 11:18:31 +0100

    On VPN

--- topics/deploy/uthsc-vpn.scm
+++ topics/deploy/uthsc-vpn.scm
@@ -9,7 +9,9 @@
 ;; Put in the hosts you are interested in here.
 (define %hosts
   (list "octopus01"
-        "tux01.genenetwork.org"))
+        "tux01.genenetwork.org"
+        "tux03.genenetwork.org"
+        "tux04.genenetwork.org"))
 
 (define (ini-file name scm)
   "Return a file-like object representing INI file with @var{name} and
